41304d7bb3656addfd9e3d597f8b4d4022162122
Sully-7 <sultan.abushabab@dcmail.ca>
2026-07-29 16:23:43 -0400

    Update main.js
    implemented story generator and UK unit conversions

--- Lab 4/Part 1/main.js
+++ Lab 4/Part 1/main.js
@@ -9,3 +9,32 @@ function randomValueFromArray(array) {
 const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const places = ["the soup kitchen", "Disneyland", "the White House"];
 const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
+
+function returnRandomStoryString() {
+  const randomCharacter = randomValueFromArray(characters);
+  const randomPlace = randomValueFromArray(places);
+  const randomEvent = randomValueFromArray(events);
+
+  return `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`;
+}
+
+function generateStory() {
+  let newStory = returnRandomStoryString();
+
+  if (customName.value !== "") {
+    newStory = newStory.replace("Bob", customName.value);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = Math.round(300 / 14) + " stone";
+    const temperature = Math.round((94 - 32) * (5 / 9)) + " Celsius";
+
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace("94 Fahrenheit", temperature);
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
+
+generateBtn.addEventListener("click", generateStory);

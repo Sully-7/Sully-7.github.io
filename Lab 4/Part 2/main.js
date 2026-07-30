11c383c44c80813aecb0b6d311dcb3ec9c1cd1e4
Sully-7 <sultan.abushabab@dcmail.ca>
2026-07-29 22:32:38 -0400

    Update main.js
    Declared image data objects array and image updater function

--- Lab 4/Part 2/main.js
+++ Lab 4/Part 2/main.js
@@ -3,3 +3,18 @@ const thumbBar = document.querySelector(".thumb-bar");
 
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
+
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" }
+];
+
+function updateDisplayedImage(src, alt) {
+  displayedImage.src = src;
+  displayedImage.alt = alt;
+}

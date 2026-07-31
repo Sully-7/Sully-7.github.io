58128e0ea8570661b9d0659f20645dee2b550df7
Sully-7 <sultan.abushabab@dcmail.ca>
2026-07-31 13:00:53 -0400

    Edited main.js,index.html,style.css

--- Lab 4/Part 4/main.js
+++ Lab 4/Part 4/main.js
@@ -1,11 +1,12 @@
-// Setup canvas
+const para = document.querySelector("p");
+let count = 0;
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-// Helper functions
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -39,10 +40,22 @@ class Ball extends Shape {
   }
 
   update() {
-    if (this.x + this.size >= width) { this.velX = -this.velX; }
-    if (this.x - this.size <= 0) { this.velX = -this.velX; }
-    if (this.y + this.size >= height) { this.velY = -this.velY; }
-    if (this.y - this.size <= 0) { this.velY = -this.velY; }
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
     this.x += this.velX;
     this.y += this.velY;
   }
@@ -70,10 +83,18 @@ class EvilCircle extends Shape {
 
     window.addEventListener("keydown", (e) => {
       switch (e.key) {
-        case "a": this.x -= this.velX; break;
-        case "d": this.x += this.velX; break;
-        case "w": this.y -= this.velY; break;
-        case "s": this.y += this.velY; break;
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
       }
     });
   }
@@ -87,10 +108,21 @@ class EvilCircle extends Shape {
   }
 
   checkBounds() {
-    if (this.x + this.size >= width) { this.x -= this.size; }
-    if (this.x - this.size <= 0) { this.x += this.size; }
-    if (this.y + this.size >= height) { this.y -= this.size; }
-    if (this.y - this.size <= 0) { this.y += this.size; }
+    if (this.x + this.size >= width) {
+      this.x -= this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.x += this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y -= this.size;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y += this.size;
+    }
   }
 
   collisionDetect() {
@@ -109,3 +141,44 @@ class EvilCircle extends Shape {
     }
   }
 }
+
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+  count++;
+  para.textContent = `Ball count: ${count}`;
+}
+
+const evilCircle = new EvilCircle(random(0, width), random(0, height));
+
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    if (ball.exists) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
+  }
+
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
+
+  requestAnimationFrame(loop);
+}
+
+loop();
